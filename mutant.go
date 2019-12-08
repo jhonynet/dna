df100d44b08d35d617480bb3fb60ec31b3c711eb
Jonathan Navarro <jnavarro@lyracons.com>
2019-12-08 19:12:41 -0300

    first commit

---
+++ mutant.go
@@ -0,0 +1,106 @@
+package mutant
+
+//represent an XY point
+type Point struct {
+	x int
+	y int
+}
+
+const (
+	// quantity of sequences found to be mutant
+	subSequencesToBeMutant int = 2
+	// quantity of repeated characters to be a mutant subsequence
+	repeatCharsToBeMutant int = 4
+)
+
+var (
+	// directions evaluated before match
+	detections int = 0
+	// found mutant subsequences in current scan
+	mutantSubsequences int = 0
+	// available directions
+	directions = [4]Point{{1, 0}, {1, 1}, {0, 1}, {-1, 1}}
+)
+
+// detect if []string of dna is mutant or human
+func isMutant(dna []string) bool {
+	// loop through Y axis
+	for row, _ := range dna {
+		// loop through X axis
+		for col, _ := range dna[0] {
+			// search in matrix starting by current Point
+			matrixSearch(dna, Point{col, row})
+			// if is mutant return instantly to avoid overprocessing
+			if mutantSubsequences == subSequencesToBeMutant {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// look mutant sequence in every direction
+func matrixSearch(dna []string, startPosition Point) {
+	for _, dir := range directions {
+		detections++
+		// get characters left in current direction
+		var leftCharsCount = getCharsLeftCount(len(dna)-1, startPosition, dir)
+		// discard current direction if chars left are less
+		// than required for match a mutant subsequence
+		if leftCharsCount < repeatCharsToBeMutant {
+			continue
+		}
+		var (
+			// current point
+			pointer   Point
+			// characters repetition
+			charCount int
+			// last tested character
+			lastChar  uint8
+		)
+		// loop trough all characters to validate concurrent repeated characters
+		for currentPos := 0; currentPos < leftCharsCount; currentPos++ {
+			// increase pointer in X direction
+			pointer.x = startPosition.x + (dir.x * currentPos)
+			// increase pointer in Y direction
+			pointer.y = startPosition.y + (dir.y * currentPos)
+			// check if last character is equal to current character
+			if lastChar == dna[pointer.y][pointer.x] {
+				// increase counter
+				charCount++
+				// validate if reach repeatCharsToBeMutant
+				if charCount >= repeatCharsToBeMutant {
+					mutantSubsequences++
+					break
+				}
+			} else {
+				// if current char is not equal to last char, reset
+				charCount = 1
+				lastChar = dna[pointer.y][pointer.x]
+			}
+		}
+
+		// if is mutant return instantly to avoid overprocessing
+		if mutantSubsequences == subSequencesToBeMutant {
+			return
+		}
+	}
+}
+
+// Get characters left in specific position
+func getCharsLeftCount(limit int, position Point, dir Point) int {
+	var left int = 0
+	// increase counter while position is not out of boundaries of grid
+	for !outOfBound(position, limit) {
+		position.x += dir.x
+		position.y += dir.y
+		left++
+	}
+
+	return left
+}
+
+// validate if Point is out of limits
+func outOfBound(point Point, limit int) bool {
+	return point.x > limit || point.x < 0 || point.y > limit || point.y < 0
+}
